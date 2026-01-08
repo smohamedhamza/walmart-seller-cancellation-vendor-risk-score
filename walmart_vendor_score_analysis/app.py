0bd34e2e3da35d0bbaadc8caee6277f1bd273ff1
smohamedhamza <sm.mohamedhamza@gmail.com>
2026-01-08 15:50:19 +0530

    product categorization path changed again

--- walmart_vendor_score_analysis/app.py
+++ walmart_vendor_score_analysis/app.py
@@ -497,8 +497,8 @@ def main():
         
         final_df[confcat_cols] = final_df[confcat_cols].fillna(0)
 
-        product_categorization_fragility = pd.read_parquet("walmart_vendor_score_analysis/product categorization nlm.parquet")
-        product_categorization_fragility_3 = pd.read_parquet("walmart_vendor_score_analysis/product categorization nlm rest.parquet")
+        product_categorization_fragility = pd.read_parquet("walmart_vendor_score_analysis/product_categorization_nlm.parquet")
+        product_categorization_fragility_3 = pd.read_parquet("walmart_vendor_score_analysis/product_categorization_nlm_rest.parquet")
 
         product_categorization_fragility_final = pd.concat(
             [product_categorization_fragility, product_categorization_fragility_3],
@@ -1978,5 +1978,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
